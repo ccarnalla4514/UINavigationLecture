67a930bca446f8393ee8daec5986104c06236df0
Christian Carnalla <ccarnalla4514@stu.d214.org>
2021-01-27 14:47:34 -0600

    Initial Commit

---
+++ UINavigationLecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UINavigationLecture
+//
+//  Created by Christian Carnalla on 1/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
